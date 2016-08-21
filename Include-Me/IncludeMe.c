3e818c6f315fd7f2aa4513d7605062b6401f311b
Gamaliel Petralba <petralba_gamaliel@yahoo.com>
2016-08-21 17:15:20 +0800

    Add files via upload

---
+++ Include-Me/IncludeMe.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "StringLinkedList.h"
+
+#define BUFF_SIZE 1000
+#define FIRST_APOS_POS 9
+#define INCLUDE "#include"
+#define DOUBLE_APOS '"'
+
+int findSecondApos(char s[]) {
+    int pos = 0;
+    int ctr = 0;
+    for(int i = 0; s[i] != '\0'; i++) {
+        if(s[i] == '"') {
+            if(ctr == 0) {
+                ctr++;
+            } else {
+                return pos;
+            }
+        } 
+        pos++;
+    }
+}
+
+bool isInclude(char s[]) {
+    int secondAposPos = findSecondApos(s);
+
+    for(int i = 0; INCLUDE[i] != '\0'; i++) {
+        if(INCLUDE[i] != s[i]) {
+            return false;
+        }
+    }
+
+    if(s[FIRST_APOS_POS] != DOUBLE_APOS && 
+       s[secondAposPos] != DOUBLE_APOS)
+    {
+        return false;
+    }
+    return true;
+}
+
+char* headerFile(char line[]) {
+    /* Determine file name */
+    int headerFileLength = findSecondApos(line) - FIRST_APOS_POS - 1;
+    char* headerFileName_dynamic = calloc(headerFileLength, sizeof(char));
+
+    for(int i = 0; i < headerFileLength; i++) {
+        headerFileName_dynamic[i] = line[FIRST_APOS_POS + 1 + i];
+    }
+    headerFileName_dynamic[headerFileLength] = '\0';
+
+    return headerFileName_dynamic;
+    /**/
+}
+
+void printHeaderContents(char prevHeaderFileName[], FILE *outputFile, LinkedList* root) {
+    bool spaceCoordinator = false;
+
+    /* Initiate header file */
+    FILE *inputFile;
+    inputFile = fopen(prevHeaderFileName, "r");
+
+    if(inputFile == NULL) {
+        perror("Error while opening the file.\n");
+        printf("%s\n", prevHeaderFileName);
+        exit(EXIT_FAILURE);
+    }
+    /**/
+
+    /* Print contents */
+    char line[BUFF_SIZE];
+    while(fgets(line, BUFF_SIZE, inputFile) != NULL) {
+        if(isInclude(line)) {
+            if(spaceCoordinator) fprintf(outputFile, "\n");
+
+            char* headerFileName = headerFile(line);
+
+            if(!isInsideList(headerFileName, root)) {
+                addList(headerFileName, root);
+                printHeaderContents(headerFileName, outputFile, root);
+                spaceCoordinator = true; 
+                fprintf(outputFile, "\n");            
+            }
+
+        } else {
+            spaceCoordinator = false;
+            fprintf(outputFile, "%s", line);
+        }
+    }
+    /**/
+
+    /* Destroy files */
+    fclose(inputFile);
+    /**/
+}
+
+void main() {
+    LinkedList root = createList();
+    bool spaceCoordinator = false;
+
+    /* Initiate files */
+    FILE *inputFile, *outputFile;
+    inputFile = fopen("inputFile.cpp", "r"); 
+    outputFile = fopen("outputFile.out", "w");
+
+    if(inputFile == NULL) {
+        perror("Error while opening the file.\n");
+        printf("inputFile.cpp");
+        exit(EXIT_FAILURE);
+    }
+
+    if(outputFile == NULL) {
+        perror("Error while opening the file.\n");
+        printf("outputFile.out");
+        exit(EXIT_FAILURE);
+    }
+
+    /**/
+
+    /* Print contents */
+    char line[BUFF_SIZE];
+    while(fgets(line, BUFF_SIZE, inputFile) != NULL) {
+        if(isInclude(line)) {
+            if(spaceCoordinator) fprintf(outputFile, "\n");
+
+            char* headerFileName = headerFile(line);
+
+            if(!isInsideList(headerFileName, &root)) {
+                addList(headerFileName, &root);
+                printHeaderContents(headerFileName, outputFile, &root);
+                spaceCoordinator = true;
+                fprintf(outputFile, "\n"); 
+            }
+ 
+        } else {
+            spaceCoordinator = false;
+            fprintf(outputFile, "%s", line);
+        }
+    }
+    /**/
+
+    /* Destroy files */
+    fclose(inputFile);
+    fclose(outputFile);
+    /**/
+}
---
+++ Include-Me/StringLinkedList.h
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct LinkedList {
+    char* listFile;
+    struct LinkedList* rest;
+} LinkedList;
+
+LinkedList createList() {
+    LinkedList root = {NULL, NULL};
+    return root;
+}
+
+void addList(char* listFile, LinkedList* root) {
+    if(root->listFile == NULL && root->rest == NULL) {
+        root->listFile = listFile;
+    } else if(root->listFile != NULL && root->rest == NULL){
+        LinkedList* next = (LinkedList*) malloc(sizeof(LinkedList));
+        next->listFile = listFile;      
+        next->rest = NULL;
+        root->rest = next;
+    } else {
+        addList(listFile, root->rest);
+    }
+}
+
+bool isInsideList(char* listFile, LinkedList* root) {
+    if(root->listFile != NULL) {
+        if(strcmp(root->listFile, listFile) == 0) {
+            return true;
+        } else {
+            if(root->rest == NULL) {
+                return false;
+            } else {
+                isInsideList(listFile, root->rest);
+            }
+        }
+    } else {
+        return false;
+    }
+}
+
+void displayList(LinkedList* root) {
+    if(root->listFile != NULL && root->rest != NULL) {
+        printf("%s\n", root->listFile);
+        displayList(root->rest);
+    } else if(root->listFile != NULL && root->rest == NULL) {
+        printf("%s\n", root->listFile);
+    } else {
+        return;
+    }
+}
+
