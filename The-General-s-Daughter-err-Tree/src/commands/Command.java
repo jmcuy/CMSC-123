70e8b602e535857f3cf8978476da7816b16d7955
Gamaliel Petralba <petralbagamaliel@gmail.com>
2016-12-11 18:21:59 +0800

    Remove folder from repository

--- The-General-s-Daughter-err-Tree/src/commands/Command.java
+++
@@ -1,8 +0,0 @@
-package commands;
-import structure.MainSimulationGui;
-
-public abstract class Command {
-	
-	public abstract void implementation(MainSimulationGui directoryFrame, String address);
-
-}
